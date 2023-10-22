87e8c5736e6f15f8c6c4fd7e8fbbebf45ad6cf28
bellDev-code <sumaoo20@naver.com>
2023-10-22 19:28:39 +0900

    particle setup

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
 window.addEventListener('load', function() {
     init()
@@ -6,7 +7,6 @@ window.addEventListener('load', function() {
 
 function init() {
     const renderer = new THREE.WebGLRenderer({
-        // alpha: true
         antialias: true
     })
     renderer.setSize(window.innerWidth, window.innerHeight)
@@ -22,13 +22,52 @@ function init() {
         1,
         500
     )
+
+    camera.position.z = 5
+
+    new OrbitControls(camera, renderer.domElement)
+
+    const geometry = new THREE.BufferGeometry()
+    
+    const count = 1000
+
+    const positions = new Float32Array(count * 3)
+    const colors = new Float32Array(count * 3)
+
+    for (let i = 0; i < count; i++) {
+        positions[i * 3] = THREE.MathUtils.randFloatSpread(10)
+        positions[i * 3 + 1] = THREE.MathUtils.randFloatSpread(10)
+        positions[i * 3 + 2] = THREE.MathUtils.randFloatSpread(10)
+
+        colors[i * 3] = Math.random()
+        colors[i * 3 + 1] = Math.random()
+        colors[i * 3 + 2] = Math.random()
+    }
+
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+    const material = new THREE.PointsMaterial({
+        color: 0xccaaff,
+        size: 0.1,
+        vertexColors: true,
+    })
+
+    const textureLoader = new THREE.TextureLoader()
+
+    const texture = textureLoader.load('../src/assets/textures/particle.png')
+
+    material.alphaMap = texture
+    material.transparent = true
+    material.depthWrite = false
+
+    const points = new THREE.Points(geometry, material)
+    scene.add(points)
     
     render()
 
     function render() {
         renderer.render(scene, camera)
-
-        controls.update()
 
         requestAnimationFrame(render)
     }
@@ -42,7 +81,6 @@ function init() {
 
         renderer.render()
 
-        controls.update()
     }
 
     window.addEventListener('resize', handleResize)
