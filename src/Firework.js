76147fd5cd383d34d970c605cca0fc765ef3e0f4
bellDev-code <sumaoo20@naver.com>
2023-10-22 22:55:59 +0900

    Firework Complete

--- src/Firework.js
+++ src/Firework.js
@@ -2,19 +2,39 @@ import * as THREE from "three"
 
 class Firework {
     constructor({x, y}) {
-        const count = 1000
+        const count = 1000 + Math.round(Math.random() * 5000)
+        const velocity = 10 + Math.random() * 10
 
         const particleGeometry = new THREE.BufferGeometry()
 
-        const particles = []
+        this.particles = []
 
         for (let i = 0; i < count; i++) {
             const particle = new THREE.Vector3(x, y, 0);
+
+            // 구면 좌표계를 직교 좌표계로 변경하는 방법
+            particle.theta = Math.random() * Math.PI * 2
+            particle.phi = Math.random() * Math.PI * 2
+
+            particle.deltaX = velocity * Math.sin(particle.theta) * Math.cos(particle.phi)
+            particle.deltaY = velocity * Math.sin(particle.theta) * Math.sin(particle.phi)
+            particle.deltaZ = velocity * Math.cos(particle.theta)
+
+            // 하나의 원 파티클 (구글 : 원의 좌표 구하기 참고)
+            // particle.theta = Math.random() * Math.PI * 2
+            // particle.deltaX = velocity * Math.cos(particle.theta)
+            // particle.deltaY = velocity * Math.sin(particle.theta)
+            // particle.deltaZ = 0
+
+            // 육각면체 파티클
+            // particle.deltaX = THREE.MathUtils.randFloatSpread(velocity)
+            // particle.deltaY = THREE.MathUtils.randFloatSpread(velocity)
+            // particle.deltaZ = THREE.MathUtils.randFloatSpread(velocity)
             
-            particles.push(particle)
+            this.particles.push(particle)
         }
 
-        particleGeometry.setFromPoints(particles)
+        particleGeometry.setFromPoints(this.particles)
 
         const textureLoader = new THREE.TextureLoader()
 
@@ -25,7 +45,8 @@ class Firework {
             alphaMap: texture,
             transparent: true,
             depthWrite: false,
-            color: new THREE.Color(Math.random(), Math.random(), Math.random())
+            color: new THREE.Color(Math.random(), Math.random(), Math.random()),
+            blending: THREE.AdditiveBlending
         })
 
         const points = new THREE.Points(particleGeometry, particleMaterial)
@@ -33,7 +54,21 @@ class Firework {
         this.points = points
     }
 
-    update() {}
+    update() {
+        const position = this.points.geometry.attributes.position
+
+        for (let i = 0; i < this.particles.length; i++) {
+            const x = position.getX(i)
+            const y = position.getY(i)
+            const z = position.getZ(i)
+
+            position.setX(i, x + this.particles[i].deltaX)
+            position.setY(i, y + this.particles[i].deltaY)
+            position.setZ(i, z + this.particles[i].deltaZ)
+        }
+
+        position.needsUpdate = true
+    }
 }
 
 export default Firework
--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,4 @@
 import * as THREE from "three"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import Firework from "./FireWork"
 
 window.addEventListener('load', function() {
@@ -21,22 +20,32 @@ function init() {
         75,
         window.innerWidth / window.innerHeight,
         1,
-        500
+        10000
     )
 
-    camera.position.z = 5
+    camera.position.z = 8000
 
-    new OrbitControls(camera, renderer.domElement)
+    const fireworks = []
+
+    fireworks.update = function() {
+        for (let i = 0; i < this.length; i++) {
+            const firework = fireworks[i]
+            
+            firework.update()
+        }
+    }
 
     const firework = new Firework({ x: 0, y: 0})
     
     scene.add(firework.points)
+
+    fireworks.push(firework)
     
     render()
 
     function render() {
-        firework.update()
-        
+        fireworks.update()
+
         renderer.render(scene, camera)
 
         requestAnimationFrame(render)
@@ -55,4 +64,16 @@ function init() {
 
     window.addEventListener('resize', handleResize)
 
+    function handleMouseDown() {
+        const firework = new Firework({
+            x: THREE.MathUtils.randFloatSpread(8000),
+            y: THREE.MathUtils.randFloatSpread(8000)
+        })
+
+        scene.add(firework.points)
+
+        fireworks.push(firework)
+    }
+
+    window.addEventListener('mousedown', handleMouseDown)
 }
